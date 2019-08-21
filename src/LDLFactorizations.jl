10261d0ca628bc233ef7b6ae747fa1c9865aebac
Théo Galizzi <theo.galizzi@etu.enseeiht.fr>
2019-08-21 17:34:12 -0400

    Allow input of the upper triangle only

--- src/LDLFactorizations.jl
+++ src/LDLFactorizations.jl
@@ -8,12 +8,61 @@ mutable struct SQDException <: Exception
   msg::String
 end
 
-function ldl_symbolic!(n, Ap, Ai, Lp, parent, Lnz, flag, P, Pinv)
-  # compute inverse permutation
-  @inbounds for k = 1:n
-    Pinv[P[k]] = k
+"""
+Compute the sparse structure of missing elements of the upper triangle of PAPt. Non zero elements have to verify Pinv[i] < Pinv[j] where i is the
+row indice and j the column indice. Those elements are the non zero of the lower triangle of A which will be in the upper triangle of PAPt (after permutation)
+# Arguments
+- `n::Ti`: number of column of the matrix
+- `Ap::Vector{Ti}`: colptr of the matrix to factorize (CSC format)
+- `Ai::Vector{Ti}`: rowval of the matrix to factorize (CSC format)
+- `Cp::Vector{Ti}`: colptr of the lower triangle (to be modified)
+- `w::Vector{Ti}`: work array
+- `Pinv::Vector{Ti}`: inverse permutation of P. PAPt is the matrix to factorize (CSC format)
+"""
+function col_symb!(n, Ap, Ai, Cp, w, Pinv)
+  fill!(w,0)
+  @inbounds for j = 1:n
+    @inbounds for p = Ap[j] : (Ap[j+1]-1)
+      i = Ai[p]
+      i >= j && break  # only upper part
+      Pinv[i] < Pinv[j] && continue  # store only what will be used during the factorization
+      w[i] += 1  # count entries
+    end
   end
 
+  Cp[1] = 1
+  @inbounds for i = 1:n  # cumulative sum
+    Cp[i+1] = w[i] + Cp[i]
+    w[i] = Cp[i]
+  end
+end
+
+"""
+Compute the rowval and values of missing elements of the upper triangle of PAPt Non zero elements have to verify Pinv[i] >= Pinv[j] where i is the
+row indice and j the column indice. Those elements are the non zero of the lower triangle of A which will be in the upper triangle of PAPt (after permutation)
+# Arguments
+- `n::Ti`: number of column of the matrix
+- `Ap::Vector{Ti}`: colptr of the matrix to factorize (CSC format)
+- `Ai::Vector{Ti}`: rowval of the matrix to factorize (CSC format)
+- `Ax::Vector{Ti}`: values of the matrix to factorize (CSC format)
+- `Cp::Vector{Ti}`: colptr of the lower triangle
+- `Ci::Vector{Ti}`: rowval of the lower triangle
+- `w::Vector{Ti}`: work array
+- `Pinv::Vector{Ti}`: inverse permutation of P. PAPt is the matrix to factorize (CSC format)
+"""
+function col_num!(n, Ap, Ai, Ax, Cp, Ci, w, Pinv)
+  @inbounds for j = 1:n
+    @inbounds for p = Ap[j] : (Ap[j+1]-1)
+      i = Ai[p]
+      i >= j  && break  # only upper part
+      Pinv[i] < Pinv[j] && continue  # store only what will be used during the factorization
+      Ci[w[i]] = j
+      w[i] += 1
+    end
+  end
+end
+
+function ldl_symbolic_upper!(n, Ap, Ai, Cp, Ci , Lp, parent, Lnz, flag, P, Pinv)
   @inbounds for k = 1:n
     parent[k] = -1
     flag[k] = k
@@ -31,6 +80,20 @@ function ldl_symbolic!(n, Ap, Ai, Lp, parent, Lnz, flag, P, Pinv)
         i = parent[i]
       end
     end
+
+    # Missing non zero elements of the upper triangle
+    @inbounds for ind = Cp[pk] : (Cp[pk+1] - 1)
+      i = Pinv[Ci[ind]]
+      i > k && continue
+      @inbounds while flag[i] != k
+        if parent[i] == -1
+          parent[i] = k
+        end
+      Lnz[i] += 1
+      flag[i] = k
+      i = parent[i]
+      end
+    end
   end
   Lp[1] = 1
   @inbounds for k = 1:n
@@ -38,7 +101,32 @@ function ldl_symbolic!(n, Ap, Ai, Lp, parent, Lnz, flag, P, Pinv)
   end
 end
 
-function ldl_numeric!(n, Ap, Ai, Ax, Lp, parent, Lnz, Li, Lx, D, Y,
+function ldl_symbolic!(n, Ap, Ai, Lp, parent, Lnz, flag, P, Pinv)
+  @inbounds for k = 1:n
+  parent[k] = -1
+  flag[k] = k
+  Lnz[k] = 0
+  pk = P[k]
+  @inbounds for p = Ap[pk] : (Ap[pk+1] - 1)
+    i = Pinv[Ai[p]]
+    i ≥ k && continue
+      @inbounds while flag[i] != k
+        if parent[i] == -1
+          parent[i] = k
+        end
+        Lnz[i] += 1
+        flag[i] = k
+        i = parent[i]
+        end
+      end
+    end
+    Lp[1] = 1
+    @inbounds for k = 1:n
+    Lp[k+1] = Lp[k] + Lnz[k]
+  end
+end
+
+function ldl_numeric_upper!(n, Ap, Ai, Ax, Cp, Ci, Lp, parent, Lnz, Li, Lx, D, Y,
                       pattern, flag, P, Pinv)
   @inbounds for k = 1:n
     Y[k] = 0
@@ -52,6 +140,75 @@ function ldl_numeric!(n, Ap, Ai, Ax, Lp, parent, Lnz, Li, Lx, D, Y,
       Y[i] += Ax[p]
       len = 1
       @inbounds while flag[i] != k
+        pattern[len] = i
+        len += 1
+        flag[i] = k
+        i = parent[i]
+      end
+      @inbounds while len > 1
+        top -= 1
+        len -= 1
+        pattern[top] = pattern[len]
+      end
+    end
+    # missing non zero elements of the upper triangle
+    @inbounds for ind = Cp[pk] : (Cp[pk+1] - 1)
+      i2 = Ci[ind]
+      i = Pinv[i2]
+      i > k && continue
+      @inbounds for p = Ap[i2] : (Ap[i2+1] - 1)
+        Ai[p] < pk && continue
+        Y[i] += Ax[p]
+        len = 1
+        @inbounds while flag[i] != k
+          pattern[len] = i
+          len += 1
+          flag[i] = k
+          i = parent[i]
+        end
+        @inbounds while len > 1
+          top -= 1
+          len -= 1
+          pattern[top] = pattern[len]
+        end
+        break
+      end
+    end
+    D[k] = Y[k]
+    Y[k] = 0
+    @inbounds while top ≤ n
+      i = pattern[top]
+      yi = Y[i]
+      Y[i] = 0
+      @inbounds for p = Lp[i] : (Lp[i] + Lnz[i] - 1)
+        Y[Li[p]] -= Lx[p] * yi
+      end
+      p = Lp[i] + Lnz[i]
+      l_ki = yi / D[i]
+      D[k] -= l_ki * yi
+      Li[p] = k
+      Lx[p] = l_ki
+      Lnz[i] += 1
+      top += 1
+    end
+    D[k] == 0 && throw(SQDException("matrix does not possess a LDL' factorization for this permutation"))
+  end
+end
+
+function ldl_numeric!(n, Ap, Ai, Ax, Lp, parent, Lnz, Li, Lx, D, Y,
+                      pattern, flag, P, Pinv)
+  @inbounds for k = 1:n
+    Y[k] = 0
+    top = n + 1
+    flag[k] = k
+    Lnz[k] = 0
+    pk = P[k]
+    @inbounds for p = Ap[pk] : (Ap[pk+1] - 1)
+      i = Pinv[Ai[p]]
+      i > k && continue
+      Y[i] += Ax[p]
+      len = 1
+      @inbounds  while flag[i] != k
         pattern[len] = i
         len += 1
         flag[i] = k
@@ -130,11 +287,11 @@ mutable struct LDLFactorization{T<:Real,Ti<:Integer}
 end
 
 # use AMD permutation by default
-ldl(A::Array{T,2}, args...) where T<:Real = ldl(sparse(A), args...)
-ldl(A::SparseMatrixCSC{T,Ti}) where {T<:Real,Ti<:Integer} = ldl(A, amd(A))
+ldl(A::Array{T,2}, args...; upper = false) where T<:Real = ldl(sparse(A), args...; upper = upper)
+ldl(A::SparseMatrixCSC{T,Ti}; upper = false ) where {T<:Real,Ti<:Integer} = ldl(A, amd(A); upper = upper)
 
 # use ldl(A, collect(1:n)) to suppress permutation
-function ldl(A::SparseMatrixCSC{T,Ti}, P::Vector{Ti}) where {T<:Real,Ti<:Integer}
+function ldl(A::SparseMatrixCSC{T,Ti}, P::Vector{Ti} ; upper = false) where {T<:Real,Ti<:Integer}
   n = size(A, 1)
   n == size(A, 2) || throw(DimensionMismatch("matrix must be square"))
   n == length(P) || throw(DimensionMismatch("permutation size mismatch"))
@@ -146,8 +303,23 @@ function ldl(A::SparseMatrixCSC{T,Ti}, P::Vector{Ti}) where {T<:Real,Ti<:Integer
   pinv = Vector{Ti}(undef, n)
   Lp = Vector{Ti}(undef, n+1)
 
-  # perform symbolic analysis
-  ldl_symbolic!(n, A.colptr, A.rowval, Lp, parent, Lnz, flag, P, pinv)
+  #Compute inverse permutation
+  @inbounds for k = 1:n
+    pinv[P[k]] = k
+  end
+
+  if upper
+    Cp = Vector{Ti}(undef, n + 1)
+    col_symb!(n, A.colptr, A.rowval, Cp, Lp, pinv)
+    Ci = Vector{Ti}(undef, Cp[end] - 1)
+    col_num!(n, A.colptr, A.rowval, A.nzval, Cp, Ci, Lp, pinv)
+
+    # perform symbolic analysis
+    ldl_symbolic_upper!(n, A.colptr, A.rowval, Cp, Ci, Lp, parent, Lnz, flag, P, pinv)
+  else
+    # perform symbolic analysis
+    ldl_symbolic!(n, A.colptr, A.rowval, Lp, parent, Lnz, flag, P, pinv)
+  end
 
   # allocate space for numerical factorization
   Li = Vector{Ti}(undef, Lp[n] - 1)
@@ -157,7 +329,11 @@ function ldl(A::SparseMatrixCSC{T,Ti}, P::Vector{Ti}) where {T<:Real,Ti<:Integer
   pattern = Vector{Ti}(undef, n)
 
   # perform numerical factorization
-  ldl_numeric!(n, A.colptr, A.rowval, A.nzval, Lp, parent, Lnz, Li, Lx, D, Y, pattern, flag, P, pinv)
+  if upper
+    ldl_numeric_upper!(n, A.colptr, A.rowval, A.nzval, Cp, Ci, Lp, parent, Lnz, Li, Lx, D, Y, pattern, flag, P, pinv)
+  else
+    ldl_numeric!(n, A.colptr, A.rowval, A.nzval, Lp, parent, Lnz, Li, Lx, D, Y, pattern, flag, P, pinv)
+  end
 
   return LDLFactorization(SparseMatrixCSC{T,Ti}(n, n, Lp, Li, Lx), D, P)
 end
