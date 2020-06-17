b04024785169f82ee49a740b224bcb65330a22bf
Dominique Orban <dominique.orban@gmail.com>
2020-06-17 16:44:36 -0400

    separate analyze/factorize phases

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,26 +43,44 @@ A = [ 0 1
       1 1 ]
 @test_throws LDLFactorizations.SQDException ldl(A, [1, 2])
 
+A = Symmetric(sparse(triu(A)))
+S = ldl_analyze(A)
+@test_throws LDLFactorizations.SQDException ldl_factorize!(A, S)
+
 # Sparse tests
 for Ti in (Int32, Int), Tf in (Float32, Float64, BigFloat)
   A = sparse(Ti[1, 2, 1, 2], Ti[1, 1, 2, 2], Tf[10, 2, 2, 5])
-  b = A * ones(Tf, 2)
+  z = ones(Tf, 2)
+  b = A * z
   LDLT = ldl(A)
   x = LDLT \ b
+  @test norm(x - z) ≤ sqrt(eps(Tf)) * norm(z)
   r = A * x - b
   @test norm(r) ≤ sqrt(eps(Tf)) * norm(b)
 
   x2 = copy(b)
   ldiv!(LDLT, x2)
+  @test norm(x2 - z) ≤ sqrt(eps(Tf)) * norm(z)
   r2 = A * x2 - b
   @test norm(r2) ≤ sqrt(eps(Tf)) * norm(b)
 
   y = similar(b)
   ldiv!(y, LDLT, b)
+  @test norm(y - z) ≤ sqrt(eps(Tf)) * norm(z)
   r2 = A * y - b
   @test norm(r2) ≤ sqrt(eps(Tf)) * norm(b)
 
   @test nnz(LDLT) == nnz(LDLT.L) + length(LDLT.d)
+
+  # test with separate analyze/factorize phases
+  # A = Symmetric(sparse(triu(A)))
+  S = ldl_analyze(A)
+  ldl_factorize!(A, S)
+  x2 = copy(b)
+  ldiv!(LDLT, x2)
+  @test norm(x2 - z) ≤ sqrt(eps(Tf)) * norm(z)
+  r2 = A * x2 - b
+  @test norm(r2) ≤ sqrt(eps(Tf)) * norm(b)
 end
 
 # Using only the upper triangle tests
@@ -80,44 +98,52 @@ A = [ 1.7     0     0     0     0     0     0     0   .13     0
 b = [.287, .22, .45, .44, 2.486, .72, 1.55, 1.424, 1.621, 3.759]
 ϵ = sqrt(eps(eltype(A)))
 
-A_upper = triu(A)
-LDLT_upper = ldl(A_upper, upper = true)
+A_upper = Symmetric(triu(A), :U)
+LDLT_upper = ldl(A_upper)
 x = LDLT_upper \ b
-
-r = A * x - b
-@test norm(r) ≤ ϵ * norm(b)
 
 y = collect(0.1:0.1:1)
 @test norm(x - y) ≤ ϵ * norm(y)
 
+r = A * x - b
+@test norm(r) ≤ ϵ * norm(b)
+
 x2 = copy(b)
 ldiv!(LDLT, x2)
+@test norm(x2 - y) ≤ ϵ * norm(y)
 
 r2 = A * x2 - b
 @test norm(r2) ≤ ϵ * norm(b)
-
-@test norm(x2 - y) ≤ ϵ * norm(y)
 
 @test nnz(LDLT_upper) == nnz(LDLT_upper.L) + length(LDLT_upper.d)
 
+# test with separate analyze/factorize phases
+# A = Symmetric(sparse(triu(A)))
+S = ldl_analyze(A)
+ldl_factorize!(A, S)
+x2 = copy(b)
+ldiv!(LDLT, x2)
+@test norm(x2 - y) ≤ ϵ * norm(y)
+r2 = A * x2 - b
+@test norm(r2) ≤ ϵ * norm(b)
+
 # test with a permutation of a different int type
 p = Int32.(collect(size(A,1):-1:1))
-LDLT_upper = ldl(A_upper, p, upper = true)
+LDLT_upper = ldl(A_upper, p)
 x = LDLT_upper \ b
-
-r = A * x - b
-@test norm(r) ≤ ϵ * norm(b)
 
 y = collect(0.1:0.1:1)
 @test norm(x - y) ≤ ϵ * norm(y)
 
+r = A * x - b
+@test norm(r) ≤ ϵ * norm(b)
+
 x2 = copy(b)
 ldiv!(LDLT, x2)
+@test norm(x2 - y) ≤ ϵ * norm(y)
 
 r2 = A * x2 - b
 @test norm(r2) ≤ ϵ * norm(b)
-
-@test norm(x2 - y) ≤ ϵ * norm(y)
 
 # Tests with multiple right-hand sides
 B = 1.0 * [ i + j for j = 1 : 10, i = 0 : 3]
@@ -129,14 +155,14 @@ ldiv!(Y, LDLT, B)
 # this matrix does not possess an LDLᵀ factorization without pivoting
 A = triu([ 0 1
            1 1 ])
-@test_throws LDLFactorizations.SQDException ldl(A, [1, 2], upper = true)
+@test_throws LDLFactorizations.SQDException ldl(A, [1, 2])
 
 # Sparse tests
 for Ti in (Int32, Int), Tf in (Float32, Float64, BigFloat)
   A = sparse(Ti[1, 2, 1, 2], Ti[1, 1, 2, 2], Tf[10, 2, 2, 5])
-  A_upper = triu(A)
+  A_upper = Symmetric(triu(A), :U)
   b = A * ones(Tf, 2)
-  LDLT = ldl(A_upper, upper = true)
+  LDLT = ldl(A_upper)
   x = LDLT \ b
   r = A * x - b
   @test norm(r) ≤ sqrt(eps(Tf)) * norm(b)
